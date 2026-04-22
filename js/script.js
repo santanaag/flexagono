b64198bb52886d56850cea1fb6ced49f7f6586b3
santanaag <adriano_gsgs@hotmail.com>
2026-04-22 07:06:35 -0300

    Update script.js
    ainda problemas com a img3.

--- js/script.js
+++ js/script.js
@@ -346,12 +346,12 @@ function processar() {
   });
   // triângulos 3,6,9,12,15,18 → imagem 3
   carregarImagem(3, img => {
-    desenharTriangulo(ctxF, img, [  unit, 2.5 * unith], 2 * unith, 5, 1);
-    desenharTriangulo(ctxF, img, [2*unit, 4.0 * unith], 2 * unith, 4, 2);
-    desenharTriangulo(ctxF, img, [     0, 1.0 * unith], 2 * unith, 5, 3);
-    desenharTriangulo(ctxF, img, [  unit, 2.5 * unith], 2 * unith, 4, 4);
-    desenharTriangulo(ctxF, img, [     0, 4.0 * unith], 2 * unith, 5, 5);
-    desenharTriangulo(ctxF, img, [2*unit, 1.0 * unith], 2 * unith, 4, 6);
+    desenharTriangulo(ctxF, img, [     0, 1.0 * unith], 2 * unith, 5, 1);
+    desenharTriangulo(ctxF, img, [  unit, 2.5 * unith], 2 * unith, 4, 2);
+    desenharTriangulo(ctxF, img, [     0, 4.0 * unith], 2 * unith, 5, 3);
+    desenharTriangulo(ctxF, img, [2*unit, 1.0 * unith], 2 * unith, 4, 4);
+    desenharTriangulo(ctxF, img, [  unit, 2.5 * unith], 2 * unith, 5, 5);
+    desenharTriangulo(ctxF, img, [2*unit, 4.0 * unith], 2 * unith, 4, 6);
   });
   // triângulos 2,5,8,11,14,17 → imagem 5
   carregarImagem(5, img => {
@@ -413,12 +413,12 @@ function processarTeste() {
     desenharTrianguloPasta(ctxF, "assets/img/img1.png", [     0, 4.0 * unith], 2 * unith, 1, 5);
     desenharTrianguloPasta(ctxF, "assets/img/img1.png", [  unit, 2.5 * unith], 2 * unith, 2, 6);
   // triângulos 3,6,9,12,15,18 → imagem 3
-    desenharTrianguloPasta(ctxF, "assets/img/img3.png", [  unit, 2.5 * unith], 2 * unith, 5, 1);
-    desenharTrianguloPasta(ctxF, "assets/img/img3.png", [2*unit, 4.0 * unith], 2 * unith, 4, 2);
-    desenharTrianguloPasta(ctxF, "assets/img/img3.png", [     0, 1.0 * unith], 2 * unith, 5, 3);
-    desenharTrianguloPasta(ctxF, "assets/img/img3.png", [  unit, 2.5 * unith], 2 * unith, 4, 4);
-    desenharTrianguloPasta(ctxF, "assets/img/img3.png", [     0, 4.0 * unith], 2 * unith, 5, 5);
-    desenharTrianguloPasta(ctxF, "assets/img/img3.png", [2*unit, 1.0 * unith], 2 * unith, 4, 6);
+    desenharTrianguloPasta(ctxF, "assets/img/img3.png", [     0, 1.0 * unith], 2 * unith, 5, 1);
+    desenharTrianguloPasta(ctxF, "assets/img/img3.png", [  unit, 2.5 * unith], 2 * unith, 4, 2);
+    desenharTrianguloPasta(ctxF, "assets/img/img3.png", [     0, 4.0 * unith], 2 * unith, 5, 3);
+    desenharTrianguloPasta(ctxF, "assets/img/img3.png", [2*unit, 1.0 * unith], 2 * unith, 4, 4);
+    desenharTrianguloPasta(ctxF, "assets/img/img3.png", [  unit, 2.5 * unith], 2 * unith, 5, 5);
+    desenharTrianguloPasta(ctxF, "assets/img/img3.png", [2*unit, 4.0 * unith], 2 * unith, 4, 6);
   // triângulos 2,5,8,11,14,17 → imagem 5
     desenharTrianguloPasta(ctxF, "assets/img/img5.png", [     0, 1.0 * unith], 2 * unith, 0, 1);
     desenharTrianguloPasta(ctxF, "assets/img/img5.png", [2*unit, 4.0 * unith], 2 * unith, 3, 2);
